3d6d92a624eb89647c17b586180299c0b52f09c2
Kanad <codewizardkanad@gmail.com>
2025-06-22 23:47:52 +0530

    Initial Setup Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, DM_Serif_Text, Mona_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,17 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const dmSerifText = DM_Serif_Text({
+  variable: "--font-dmserif",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${dmSerifText.variable} ${monaSans.variable} antialiased`}
       >
         {children}
       </body>
