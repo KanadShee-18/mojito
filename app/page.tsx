7d273603a0383e42d05f8f77eff22874fb06ace2
Kanad <codewizardkanad@gmail.com>
2025-06-23 10:43:19 +0530

    Implemented the cocktail section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { HeroSection } from "@/components/hero-section";
 import { Navbar } from "@/components/navbar-section";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { Cocktail } from "@/components/cocktail-section";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const page = () => {
     <main>
       <Navbar />
       <HeroSection />
+      <Cocktail />
     </main>
   );
 };
